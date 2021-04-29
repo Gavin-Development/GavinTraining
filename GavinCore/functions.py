6212422f03e90d770473bf18ad02d5776ca718da
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-04-29 23:55:06 +0100

    Removed loss function.

--- GavinCore/functions.py
+++ GavinCore/functions.py
@@ -2,18 +2,6 @@ from GavinCore import tf, tfds
 from GavinCore.preprocessing.text import preprocess_sentence
 
 
-def loss_function(y_true, y_pred, max_len: int = 52):
-    y_true = tf.reshape(y_true, shape=(-1, max_len - 1))
-
-    loss = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction='none')(y_true, y_pred)
-
-    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
-    loss = tf.multiply(loss, mask)
-
-    return tf.reduce_mean(loss)
-
-
 def evaluate(sentence: str, model: tf.keras.models.Model, max_len: int, s_token: list, e_token: list, tokenizer: tfds.deprecated.text.SubwordTextEncoder):
     sentence = preprocess_sentence(sentence)
 
