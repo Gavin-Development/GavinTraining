bc684af32f29706f22ab0544e7062c1669b6386c
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-05-03 23:34:02 +0100

    Adjust main.py layout.

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ from GavinBackend.DataParsers.load_data import load_tokenized_data
 
 LOG_DIR = './bunchOfLogs'
 MODEL_NAME = input("Please enter Model_Name: ")
+MAX_SAMPLES = int(input("MAX_SAMPLES: "))
+BUFFER_SIZE = 20_000
 if os.path.exists(os.path.join(LOG_DIR, MODEL_NAME)):
     model = TransformerIntegration.load_model(LOG_DIR, MODEL_NAME)
 else:
-    MAX_SAMPLES = int(input("MAX_SAMPLES: "))
-    BATCH_SIZE = int(input("BATCH_SIZE(32): "))
-    BUFFER_SIZE = 20_000
-    MAX_LENGTH = 40 + 2
-    NUM_LAYERS = int(input("Please enter the number of NUM_LAYERS(4): "))
-    D_MODEL = int(input("Please enter the d_model(256): "))
-    NUM_HEADS = int(input("Please enter the NUM_HEADS(8): "))
-    UNITS = int(input("Please enter the number of units(512): "))
-    DROPOUT = float(input("Please enter the DROPOUT(0.175): "))
-    EPOCHS = int(input("Please enter the number of epochs(15): "))
+
+    BATCH_SIZE = int(input("BATCH_SIZE: "))
+    MAX_LENGTH = int(input("MAX_LENGTH: "))
+    NUM_LAYERS = int(input("Please enter the number of NUM_LAYERS: "))
+    D_MODEL = int(input("Please enter the d_model: "))
+    NUM_HEADS = int(input("Please enter the NUM_HEADS: "))
+    UNITS = int(input("Please enter the number of units: "))
+    DROPOUT = float(input("Please enter the DROPOUT: "))
+    EPOCHS = int(input("Please enter the number of epochs: "))
     TOKENIZER_PATH = input("Please enter the Tokenizer Path: ")
     tokenizer = tfds.deprecated.text.SubwordTextEncoder.load_from_file(TOKENIZER_PATH)
     model = TransformerIntegration(num_layers=NUM_LAYERS, units=UNITS, d_model=D_MODEL,
@@ -32,3 +33,4 @@ else:
     questions = tf.keras.preprocessing.sequence.pad_sequences(questions, maxlen=model.max_len, padding='post')
     answers = tf.keras.preprocessing.sequence.pad_sequences(answers, maxlen=model.max_len, padding='post')
     dataset_train, dataset_val = create_data_objects(questions, answers, buffer_size=BUFFER_SIZE, batch_size=BATCH_SIZE)
+    model.fit(dataset_train, validation_dataset=dataset_val, epochs=EPOCHS)
