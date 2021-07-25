dab02f401f362b5b8b54e3dae2d8c5ecab5305b3
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-07-25 17:24:06 +0100

    Fix loss function to work with new dataset setup.

--- GavinCore/models.py
+++ GavinCore/models.py
@@ -135,10 +135,10 @@ class TransformerAbstract(abc.ABC):
                             log_dir=self.log_dir, wrapper_model=self)]
 
     def loss_function(self, y_true, y_pred) -> tf.Tensor:
-        y_true = tf.reshape(y_true, shape=(-1, self.max_len-1))
+        y_true = tf.reshape(y_true, shape=(-1, self.max_len))
 
         loss = tf.keras.losses.SparseCategoricalCrossentropy(
-            from_logits=True, reduction='none')(y_true, y_pred[:, 1:])
+            from_logits=True, reduction='none')(y_true, y_pred)
 
         mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
         loss = tf.multiply(loss, mask)
