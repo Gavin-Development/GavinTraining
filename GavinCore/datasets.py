2d5308c3c91724ec2d56b0381b7b7a0c163d939c
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-09-09 18:21:50 +0100

    Formatting changes.

--- GavinCore/datasets.py
+++ GavinCore/datasets.py
@@ -12,12 +12,13 @@ class DatasetAPICreator:
         self.vocab_size = vocab_size
 
     def change_to_probabilities(self, first_part, second_part):
-        outputs = second_part['outputs'].eval(session=tf.compat.v1.Session(), feed_dict={second_part['outputs'].name: np.zeros(shape=(self.batch_size, 52), dtype=np.int32)})
+        outputs = second_part['outputs'].eval(session=tf.compat.v1.Session(), feed_dict={
+            second_part['outputs'].name: np.zeros(shape=(self.batch_size, 52), dtype=np.int32)})
         new_outputs = np.zeros(shape=(self.batch_size, outputs.shape[1], self.vocab_size), dtype=np.int32)
-        for sentence in range(0, self.batch_size-1):
-            for Index in range(0, new_outputs.shape[0]-1):
-                vocab_id = outputs[sentence][Index]-1
-                if vocab_id == self.vocab_size-1:
+        for sentence in range(0, self.batch_size - 1):
+            for Index in range(0, new_outputs.shape[0] - 1):
+                vocab_id = outputs[sentence][Index] - 1
+                if vocab_id == self.vocab_size - 1:
                     break
                 new_outputs[sentence][Index][vocab_id] = 1
         second_part = {'outputs': new_outputs}
@@ -50,7 +51,6 @@ class DatasetAPICreator:
         dataset_all = dataset_all.prefetch(tf.data.experimental.AUTOTUNE)
         options = tf.data.Options()
         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-        dataset_all.with_options(options)
 
         dataset_t = dataset_all.take(int(len(self.questions_train)*.8))
         dataset_v = dataset_all.skip(int(len(self.questions_train)*.8))
