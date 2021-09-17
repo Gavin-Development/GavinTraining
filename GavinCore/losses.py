1e8f3b41864df8e63282513a8650ddfb9e408334
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-09-17 22:44:20 +0100

    Updates to loss function to make it more accurate.

--- GavinCore/losses.py
+++ GavinCore/losses.py
@@ -6,8 +6,9 @@ def SparseCategoricalCrossentropy(y_true: tf.Tensor, y_pred: tf.Tensor, numeric_
     batch_size = tf.cast(tf.shape(y_true)[0], tf.float32)
     y_true = tf.cast(tf.add(y_true, numeric_stabilizer), tf.float32)
 
-    y_pred = tf.reshape(y_pred, shape=(tf.shape(y_pred)[tf.rank(y_pred) - 1], -1))
-    y_true = tf.reshape(y_true, shape=(-1,))
+    # y_pred = tf.reshape(y_pred, shape=(tf.shape(y_pred)[tf.rank(y_pred) - 1], -1))
+    # y_true = tf.reshape(y_true, shape=(-1,))
+    y_true = tf.reshape(y_true, shape=(tf.shape(y_true)[0], tf.shape(y_true)[1], 1))
     return - (tf.reduce_sum(
             tf.multiply(y_true,
                         tf.math.log(y_pred), name="mul_1")
