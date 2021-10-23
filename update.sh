7a69e16c913b9e2599b63e9b851c48f314604a2c
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-10-23 19:51:28 +0100

    Update the update scripts.

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 echo "Updating code base."
 git fetch origin --prune
+git pull https://github.com/Gavin-Development/GavinTraining master
 git submodule init
 git submodule update
 echo "Code base up to date."
 echo "Updating requirements"
 pip3 install -r requirements.txt
-cd GavinBackend && pip3 install -r requirements.txt || exit
+cd GavinBackend && pip3 install -r requirements.txt && git pull https://github.com/Gavin-Development/GavinBackend main || exit
 echo "Requirements up to date."
