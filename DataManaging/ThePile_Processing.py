7aa9de30844bbdfb8e54fc4acda25c81a9383400
Scot_Survivor <unconfigured@null.spigotmc.org>
2023-01-11 01:29:18 +0000

    [IMPROVEMENT] Initial Commit of code for processing ThePile dataset.
    Scot_Survivor

--- DataManaging/ThePile_Processing.py
+++ DataManaging/ThePile_Processing.py
@@ -1,4 +1,5 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import io
 import zstandard
 import sys
@@ -9,16 +10,18 @@ import base64
 
 import tensorflow_datasets as tfds
 from concurrent.futures import ProcessPoolExecutor
-from itertools import combinations
 
-filepath = sys.argv[1]
+filepath = None if len(sys.argv) < 2 else sys.argv[1]
+if filepath is None:
+    print("No filepath specified")
+    sys.exit(-1)
 f = open(filepath, 'rb')
 dctx = zstandard.ZstdDecompressor(max_window_size=2147483648)
 stream_reader = dctx.stream_reader(f, read_size=int(1.953e+6))
 text_stream = io.TextIOWrapper(stream_reader, encoding="utf-8")
 tokenizer = tfds.deprecated.text.SubwordTextEncoder.load_from_file("../Tokenizer-3")
-handle_to = open("C:/Users/jkshi/Datasets/Tokenizer-3-to", 'wb')
-handle_from = open("C:/Users/jkshi/Datasets/Tokenizer-3-from", 'wb')
+handle_to = open("D:/Datasets/ThePile/Tokenizer-3-to", 'wb')
+handle_from = open("D:/Datasets/ThePile/Tokenizer-3-from", 'wb')
 if __name__ == "__main__":
     for row in tqdm.tqdm(text_stream, unit=" rows", desc="Processing"):
         row = json.loads(row)
@@ -30,6 +33,11 @@ if __name__ == "__main__":
             results = list(results)
             executor.shutdown()
         results = list(map(lambda x: base64.b64encode(pickle.dumps(x)), results))
-        for to_text, from_text in combinations(results, 2):
-            handle_to.write(to_text + b'\n')
-            handle_from.write(from_text + b'\n')
+        # Select in batches of 2
+        for i in range(0, len(results), 2):
+            if (i + 1) < len(results):
+                handle_to.write(results[i])
+                handle_from.write(results[i + 1])
+            else:
+                handle_to.write(results[i])
+                handle_from.write(results[i])
