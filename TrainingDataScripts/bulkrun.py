e318e887bfd06393a866ea83b7c39b5d325b6af3
Scot_Survivor <unconfigured@null.spigotmc.org>
2023-01-11 01:32:02 +0000

    [IMPROVEMENT] Update paths
    Scot_Survivor

--- TrainingDataScripts/bulkrun.py
+++ TrainingDataScripts/bulkrun.py
@@ -6,15 +6,15 @@ import os
 import sqlite3
 
 
-timeframes = glob.glob("D:/Datasets/reddit_data/databases/*.db")
+timeframes = glob.glob("D:/Datasets/reddit/databases/*.db")
 timeframes = [os.path.basename(timeframe) for timeframe in timeframes]
 total_rows = 0
 count_total = 0
 is_counting = True
 is_select = False
-sql_main = """SELECT COUNT(c.id) FROM comment as c INNER JOIN comment AS parent ON c.parent_id = parent.id;"""
+sql_main = """SELECT COUNT(c.id) FROM comment as c;"""
 for timeframe in timeframes:
-    connection = sqlite3.connect('D:/Datasets/reddit_data/databases/{}'.format(timeframe))
+    connection = sqlite3.connect('D:/Datasets/reddit/databases/{}'.format(timeframe))
     cursor = connection.cursor()
     try:
         count = 0
